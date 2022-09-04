d46212dca5dce871cce594cecb3358c807053920
MarcioAndre3767 <marcioandre.ti1@gmail.com>
2022-09-04 09:18:21 -0300

    Integração das classes cliente tipo-cliente e endereços

--- src/main/java/com/devmam/ModelagemApplication.java
+++ src/main/java/com/devmam/ModelagemApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devmam.domain.Categoria;
 import com.devmam.domain.Cidade;
+import com.devmam.domain.Cliente;
+import com.devmam.domain.Endereco;
 import com.devmam.domain.Estado;
 import com.devmam.domain.Produto;
+import com.devmam.domain.enums.TipoCliente;
 import com.devmam.repositories.CategoriaRepository;
 import com.devmam.repositories.CidadeRepository;
+import com.devmam.repositories.ClienteRepository;
+import com.devmam.repositories.EnderecoRepository;
 import com.devmam.repositories.EstadoRepository;
 import com.devmam.repositories.ProdutoRespository;
 
@@ -30,6 +35,12 @@ public class ModelagemApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;	
 	
 	
 	public static void main(String[] args) {
@@ -69,7 +80,19 @@ public class ModelagemApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null,"Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 	}
 
